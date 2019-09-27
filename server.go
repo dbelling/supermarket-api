1616435645a8e9c8c652cfc6055206c500f8b3c2
Dan Belling <danhbelling@gmail.com>
2019-09-27 07:33:55 -0500

    Add Create Food method and unit test.

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -10,9 +11,9 @@ import (
 )
 
 type Food struct {
-	Code  string
-	Name  string
-	Price string
+	ProduceCode string
+	Name        string
+	UnitPrice   float64
 }
 
 var Produce []Food
@@ -31,7 +32,7 @@ func ShowFood(w http.ResponseWriter, r *http.Request) {
 		code := vars["code"]
 		var foundFood Food
 		for index, food := range Produce {
-			if food.Code == code {
+			if food.ProduceCode == code {
 				foundFood = Produce[index]
 				break
 			}
@@ -40,12 +41,32 @@ func ShowFood(w http.ResponseWriter, r *http.Request) {
 	}()
 
 	foundFood := <-handlerChannel
-	if foundFood.Code != "" {
+	if foundFood.ProduceCode != "" {
 		w.WriteHeader(http.StatusOK)
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 	}
 	json.NewEncoder(w).Encode(foundFood)
+}
+
+func CreateFood(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("POST /food: CreateFood")
+	handlerChannel := make(chan Food)
+
+	go func() {
+		var createdFood Food
+		reqBody, _ := ioutil.ReadAll(r.Body)
+		json.Unmarshal(reqBody, &createdFood)
+		// update our global Articles array to include
+		// our new Article
+		Produce = append(Produce, createdFood)
+		handlerChannel <- createdFood
+	}()
+
+	foodCreated := <-handlerChannel
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(foodCreated)
 }
 
 func DeleteFood(w http.ResponseWriter, r *http.Request) {
@@ -57,7 +78,7 @@ func DeleteFood(w http.ResponseWriter, r *http.Request) {
 		code := vars["code"]
 		var deletedFood bool = false
 		for index, food := range Produce {
-			if food.Code == code {
+			if food.ProduceCode == code {
 				Produce = append(Produce[:index], Produce[index+1:]...)
 				deletedFood = true
 				break
@@ -78,6 +99,7 @@ func requestHandler() {
 	produceRouter := mux.NewRouter().StrictSlash(true)
 	produceRouter.HandleFunc("/food/{code}", ShowFood)
 	produceRouter.HandleFunc("/food", ShowFoods)
+	produceRouter.HandleFunc("/foods", CreateFood).Methods("POST")
 	produceRouter.HandleFunc("/food/{code}", DeleteFood).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":9000", produceRouter))
 }
@@ -85,10 +107,10 @@ func requestHandler() {
 func main() {
 	// Instantitate in-memory database array of food.
 	Produce = []Food{
-		Food{Code: "A12T-4GH7-QPL9-3N4M", Name: "Lettuce", Price: "$3.46"},
-		Food{Code: "E5T6-9UI3-TH15-QR88", Name: "Peach", Price: "$2.99"},
-		Food{Code: "YRT6-72AS-K736-L4AR", Name: "Green Pepper", Price: "$0.79"},
-		Food{Code: "TQ4C-VV6T-75ZX-1RMR", Name: "Gala Apple", Price: "$3.59"},
+		Food{ProduceCode: "A12T-4GH7-QPL9-3N4M", Name: "Lettuce", UnitPrice: 3.64},
+		Food{ProduceCode: "E5T6-9UI3-TH15-QR88", Name: "Peach", UnitPrice: 2.99},
+		Food{ProduceCode: "YRT6-72AS-K736-L4AR", Name: "Green Pepper", UnitPrice: 0.79},
+		Food{ProduceCode: "TQ4C-VV6T-75ZX-1RMR", Name: "Gala Apple", UnitPrice: 3.59},
 	}
 	requestHandler()
 }
--- server_test.go
+++ server_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -50,5 +52,36 @@ func TestDeleteFood(t *testing.T) {
 	if status := rr.Code; status != http.StatusNotFound {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusNotFound)
+	}
+}
+
+func TestCreateFood(t *testing.T) {
+	var foodPayload = []byte(`{"ProduceCode":"ABCD-1234-DEFG-5678","Name":"Cucumber","UnitPrice":1.99}`)
+
+	req, err := http.NewRequest("POST", "/foods", bytes.NewBuffer(foodPayload))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(CreateFood)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated)
+	}
+
+	var m map[string]interface{}
+	json.Unmarshal(rr.Body.Bytes(), &m)
+	if m["ProduceCode"] != "ABCD-1234-DEFG-5678" {
+		t.Errorf("Expected ProduceCode to be 'ABCD-1234-DEFG-5678'. Got '%v'", m["ProduceCode"])
+	}
+	if m["Name"] != "Cucumber" {
+		t.Errorf("Expected Name to be 'Cucumber'. Got '%v'", m["Name"])
+	}
+	// the id is compared to 1.0 because JSON unmarshaling converts numbers to
+	// floats, when the target is a map[string]interface{}
+	if m["UnitPrice"] != 1.99 {
+		t.Errorf("Expected Price to be '1.99'. Got '%v'", m["UnitPrice"])
 	}
 }
